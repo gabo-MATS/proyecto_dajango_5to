2530c868c2db4c739d0bfad7916ac42c2f964758
Erick Castillo <castilloherrera@yahoo.com>
2017-04-17 14:49:18 -0600

    "avance"
    Gab aiuda

--- iPet/Animals/models.py
+++ iPet/Animals/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+class precio(models.Model):
+class Mascota(models.Model):
 
-# Create your models here.
+	precio = models.Charfield(max_length=100)
+	tamano = models.Charfield(max_length=100)
+	mantenimiento = models.Charfield(max_length=100)
+	convivencia = models.Charfield(max_length=100)
+	vida = models.Charfield(max_length=100)
+	ruido = models.Charfield(max_length=100)
+	carisma = models.Charfield(max_length=100)
+
+class Persona(models.Model):
+	edad = models.Charfield(max_length=100)
+	alergia = models.Charfield(max_length=100)
+	responsabilidad = models.Charfield(max_length=100)
+	dinero = models.Charfield(max_length=100)
