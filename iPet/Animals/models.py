b513b459ac12e40ea7205f1614d7ef8de20cd972
ericka1599 <erickasegura96@gmail.com>
2017-04-19 08:40:39 -0600

    :)

--- iPet/Animals/models.py
+++ iPet/Animals/models.py
@@ -1,17 +1,25 @@
 from django.db import models
-class precio(models.Model):
+
 class Mascota(models.Model):
+	mascota = models.ForeingKey()
 
-	precio = models.Charfield(max_length=100)
-	tamano = models.Charfield(max_length=100)
-	mantenimiento = models.Charfield(max_length=100)
-	convivencia = models.Charfield(max_length=100)
-	vida = models.Charfield(max_length=100)
-	ruido = models.Charfield(max_length=100)
-	carisma = models.Charfield(max_length=100)
+class Precio (models.Model):
+	precio = models.ForeignKey()
 
-class Persona(models.Model):
-	edad = models.Charfield(max_length=100)
-	alergia = models.Charfield(max_length=100)
-	responsabilidad = models.Charfield(max_length=100)
-	dinero = models.Charfield(max_length=100)
+class Tamano (models.Model):
+	tamano = models.ForeignKey()
+
+class Mantenimiento (models.Model):
+	mantenimiento = models.ForeignKey()
+
+class Convivencia (models.Model):
+	convivencia = models.ForeignKey()
+
+
+class Ruido (models.Model):
+	ruido = models.ForeignKey()
+
+
+class Carisma (models.Model):
+	carisma = models.ForeignKey()
+
