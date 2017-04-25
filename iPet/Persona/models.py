3a0f42f37fd141e79ff0158b127db18251d12ec4
gab <gabrielmartinez1222@gmail.com>
2017-04-25 17:46:16 -0600

    animales templates

--- iPet/Persona/models.py
+++ iPet/Persona/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 #Models Personas
 class Persona(models.Model):
-	edad = models.Charfield(max_length=100)
-	alergia = models.Charfield(max_length=100)
-	responsabilidad = models.Charfield(max_length=100)
-	dinero = models.ForeignKey(Income)
+	edad = models.CharField(max_length=100)
+	alergia = models.CharField(max_length=100)
+	responsabilidad = models.CharField(max_length=100)
+	#dinero = models.ForeignKey(Income)
 # Create your models here.
 
-class Income(models.Model):
+#class Income(models.Model):
+#	presupuesto=models.IntergerField()
 	
