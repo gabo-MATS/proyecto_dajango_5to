16b6ce3c630524e15c974f9236451370e1bd572c
gab <gabrielmartinez1222@gmail.com>
2017-04-26 14:49:12 -0600

    avance

--- iPeeet/animals/models.py
+++ iPeeet/animals/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Animals (models.Model):
 	price = models.IntegerField(default=0)
-	size = models.IntegerField(default=0)
+	size = models.CharField(max_length=20)
 	maintenance = models.IntegerField(default=0)
 	life = models.IntegerField(default=0)
-	
+	time = models.IntegerField(default=0)
+	noise = models.IntegerField(default=0)
+	rare = models.CharField(max_length=20)
--- iPeeet/person/models.py
+++ iPeeet/person/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+class Person(models.Model):	
+	age = models.IntegerField(default=0)
+	alergies = models.CharField(max_length=200)
+	responsable = models.IntegerField(default=0)
+	income = models.IntegerField(default=0)
