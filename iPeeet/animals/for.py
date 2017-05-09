f753a15274ed6ab707f22dd42e978c889f2e6875
ericka1599 <erickasegura96@gmail.com>
2017-05-09 11:22:01 -0600

    cambio

---
+++ iPeeet/animals/for.py
@@ -0,0 +1,4 @@
+for i in [0,1,2,3]:
+	print(i)
+	x = i**2
+	print("x vale", x)
---
+++ iPeeet/texto.py
@@ -0,0 +1,9 @@
+lista=[]
+i=0
+while i=1:
+	try:
+		x=int(input("numero"))
+	except:
+		print("ingrese un numero valido")
+	continue
+lista.append(x)
